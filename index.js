bb17796db2b268f7ad2638574d7446595e2e20d2
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2026-01-05 09:31:09 -0800

    select elements

---
+++ index.js
@@ -0,0 +1,15 @@
+let leftEye = document.getElementById('left-eye');
+let rightEye = document.getElementById('right-eye');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+
+let leftEyeTop = 80;
+let leftEyeLeft = 114;
+let leftEyePaddingLeft = 0;
+let leftEyePaddingTop = 0;
+
+
+let rightEyeTop = 80;
+let rightEyeLeft = 232;
+let rightEyePaddingLeft = 0;
+let rightEyePaddingTop = 0;
